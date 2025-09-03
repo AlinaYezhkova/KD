0a36ea9ceed3e42e23e54158d1d7363a9c5eef08
AlinaYezhkova <alinayezhkova@gmail.com>
2025-09-03 13:29:04 +0300

    lookup context functions

--- constants.h
+++ constants.h
@@ -4,7 +4,7 @@
 
 static constexpr size_t kBucketCount = 128;
 static constexpr size_t kBucketSize  = 8;
-static constexpr size_t kAlpha       = 3;
+static constexpr size_t kAlpha       = 1;
 static constexpr size_t kReturn      = 20;
 static constexpr size_t IPv4Size     = 4;
 
--- lookupcontext.cpp
+++ lookupcontext.cpp
@@ -9,38 +9,38 @@
 //     node_.find_closest(target_);
 // }
 
-// void LookupContext::sendQuery(const PeerInfo& pi) {
-//     Message out = MessageBuilder()
-//                       .type(MessageType::Find_node_query)
-//                       .from(peer_.getPeerInfo())  // current node ID
-//                       .to(pi)                     // where it's going
-//                       .find(target_)              // who we're looking for
-//                       .nonce(nonce_)
-//                       .build();
-//     peer_.send(out);
-// }
+void LookupContext::sendQuery(const PeerInfo& pi) {
+    Message out = MessageBuilder()
+                      .type(MessageType::Find_node_query)
+                      .from(peer_.getPeerInfo())  // current node ID
+                      .to(pi)                     // where it's going
+                      .find(target_)              // who we're looking for
+                      .nonce(nonce_)
+                      .build();
+    peer_.send(out);
+}
 
-// void LookupContext::onDone() {
-//     for (const auto& [id, pi] : closest_peers_) {
-//         node_.insert(pi);
-//     }
-//     fmt::println("!!! going to erase '{}' context", nonce_);
-//     peer_.endLookup(nonce_);
-// }
-// bool LookupContext::shouldStop(
-//     std::map<NodeId, PeerInfo, Comparator> current_best) {
-//     fmt::println("current_best: {} (target: {})", current_best.size(),
-//     target_); for (auto& e : current_best) {
-//         fmt::println("{} ", e.first);
-//     }
-//     if (current_best.count(target_) > 0) {
-//         fmt::println("yeah! {} found myself {}",
-//                      peer_.getPeerInfo().key_,
-//                      target_);
-//         return true;
-//     }
-//     return current_best.size() >= kReturn;  // or use convergence logic
-// }
+void LookupContext::onDone() {
+    finished_ = true;
+    for (const auto& [id, pi] : ordered_peers_) {
+        node_.insert(pi);
+    }
+    // fmt::println("!!! going to erase '{}' context", nonce_);
+    peer_.endLookup(nonce_);
+}
+
+bool LookupContext::shouldStop(
+    std::map<NodeId, PeerInfo, Comparator> current_best) {
+    // fmt::println("current_best: {} (target: {})", current_best.size(),
+    // target_); for (auto& e : current_best) {
+    //     fmt::println("{} ", e.first);
+    // }
+    if (current_best.count(target_) > 0) {
+        fmt::println("yeah");
+        return true;
+    }
+    return current_best.size() >= kReturn;  // or use convergence logic
+}
 
 // void LookupContext::issueNext() {
 //     maybeFinish();
@@ -101,15 +101,7 @@ void LookupContext::onResponse(std::vector<PeerInfo> result) {
     for (const auto& pi : unqueried) {
         queried_.insert(pi.key_);
         ++inflight_;
-
-        Message out = MessageBuilder()
-                          .type(MessageType::Find_node_query)
-                          .from(peer_.getPeerInfo())  // current node ID
-                          .to(pi)                     // where it's going
-                          .find(target_)              // who we're looking for
-                          .nonce(nonce_)
-                          .build();
-        peer_.send(out);
+        sendQuery(pi);
         if (++launched >= kAlpha) {
             break;
         }
@@ -122,38 +114,16 @@ void LookupContext::maybeFinish() {
     }
     auto   start        = ordered_peers_.begin();
     size_t closest_size = std::min(kReturn, ordered_peers_.size());
-
-    auto closest_end = start;
+    auto   closest_end  = start;
     std::advance(closest_end, closest_size);
 
     std::map<NodeId, PeerInfo, Comparator> closest_peers(comp_);
     closest_peers.insert(start, closest_end);
 
-    bool b = false;
-    if (closest_peers.count(target_) > 0) {
-        fmt::println("yeah");
-        b = true;
+    if (shouldStop(closest_peers)) {
+        onDone();
     }
-
-    if (closest_peers.size() == kReturn) {
-        b = true;
-    }
-
-    if (b) {
-        finished_ = true;
-        for (const auto& [id, pi] : ordered_peers_) {
-            node_.insert(pi);
-        }
-        // fmt::println("!!! going to erase '{}' context", nonce_);
-        peer_.endLookup(nonce_);
-    }
-
     if (inflight_ == 0 && queried_.size() >= kReturn) {
-        finished_ = true;
-        for (const auto& [id, pi] : ordered_peers_) {
-            node_.insert(pi);
-        }
-        // fmt::println("!!! going to erase '{}' context", nonce_);
-        peer_.endLookup(nonce_);
+        onDone();
     }
 }
