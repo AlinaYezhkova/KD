4073d76e14035b5d81d0290433dde27dec8fe743
AlinaYezhkova <alinayezhkova@gmail.com>
2025-09-17 14:21:41 +0300

    mainwin removed

--- MainWin.cpp
+++ MainWin.cpp
@@ -1,11 +1,11 @@
-#include "MainWin.h"
-#include "./ui_MainWin.h"
+// #include "MainWin.h"
+// #include "./ui_MainWin.h"
 
-MainWin::MainWin(QWidget* parent) : QMainWindow(parent), ui_(new Ui::MainWin) {
-    ui_->setupUi(this);
-}
+// MainWin::MainWin(QWidget* parent) : QMainWindow(parent), ui_(new Ui::MainWin) {
+//     ui_->setupUi(this);
+// }
 
-MainWin::~MainWin()
-{
-    delete ui_;
-}
+// MainWin::~MainWin()
+// {
+//     delete ui_;
+// }
